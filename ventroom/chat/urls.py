71643a4744c48c922d2a70234b324f1ac3ea0d3a
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2021-05-08 20:06:52 +0530

    fix url routes

--- ventroom/chat/urls.py
+++ ventroom/chat/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.home, name='home'),
-    path('',views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('',views.home, name='home'),
+    path('chat/',views.index, name='index'),
+    path('chat/<str:room_name>/', views.room, name='room'),
 ]
